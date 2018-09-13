8302a73c4df5a9aa60cc4762fadd78143dc161c4
yck <yck@yckdeiMac.local>
2018-09-13 18:30:40 +0800

    upload

---
+++ week11/work/public/javascripts/readfile.js
@@ -0,0 +1,12 @@
+  function createReader(cb) {
+    return function(event) {
+      const file = event.target.files[0];
+      const fr = new FileReader();
+      fr.onload = function() {
+        var img = new Image();
+        img.src = fr.result;
+        cb(img);
+      }
+      fr.readAsDataURL(file);
+    }
+  }
--- week11/work/routes/index.js
+++ week11/work/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+var util = require("util");
+var writeFile = util.promisify(fs.writeFile);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+var pic = require("../pic");
+router.post("/addPic",function(req,res){
+  pic.push(req.body);
+  fs.writeFile("./pic.json",JSON.stringify(pic),function(err){})
+  res.send(req.body);
+})
 module.exports = router;
