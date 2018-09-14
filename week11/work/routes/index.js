b25b900fe61e50beb2be25c4afa83fccc1066741
yck <yck@yckdeiMac.local>
2018-09-14 11:38:42 +0800

    week11 work

--- week11/work/routes/index.js
+++ week11/work/routes/index.js
@@ -5,13 +5,18 @@ var util = require("util");
 var writeFile = util.promisify(fs.writeFile);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: '相册'});
+// });
 var pic = require("../pic");
+router.get("/initPic",function(req,res){
+  res.send(pic);
+})
 router.post("/addPic",function(req,res){
+  console.log(req.body);
   pic.push(req.body);
   fs.writeFile("./pic.json",JSON.stringify(pic),function(err){})
   res.send(req.body);
+  return
 })
 module.exports = router;
